1951e6b4221231651b4f20b6b00a0bb0d4c154bd
Arek Burdach <arek.burdach@gmail.com>
2015-03-23 23:17:53 +0100

    traits

--- src/TraitsInheritance.scala
+++ src/TraitsInheritance.scala
@@ -4,19 +4,11 @@ trait Logging {
   }
 }
 
-case class User(
-  firstName: String,
-  optionalSecondName: Option[String],
-  lastName: String
-) 
-
-object NameLengthComputer extends Logging {
-  def fullNameLength(user: User) = {
-    val separatorBeforeLastLen = 1
-    val secondWithSeparatorLen = user.optionalSecondName.map { secondName =>
-      log(s"User $user has second name")
-      1 + secondName.length
-    }.getOrElse(0)
-    user.firstName.length + secondWithSeparatorLen + separatorBeforeLastLen + user.lastName.length
+object Worker extends Logging {
+  var i = 0
+  def doWork = {
+    i += 1
+    log(s"i is now: $i")
+    i
   }
 }
